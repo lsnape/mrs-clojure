30bafc719611177573be1988bb066465218a01f2
Luke Snape <ldjsnape@gmail.com>
2016-02-04 21:01:38 +0000

    Add metrics deps

--- project.clj
+++ project.clj
@@ -7,6 +7,14 @@
                  [environ "1.0.2"]
                  [manifold "0.1.2-alpha2"]
                  [medley "0.7.1"]
+
+                 ;; is the graphite connection exclusive to metrics-clojure-graphite,
+                 ;; and if so, can we exclude metrics-graphite and bump everything else up?
+                 [metrics-clojure "2.3.0"]
+                 [metrics-clojure-graphite "2.3.0"]
+                 [metrics-clojure-jvm "2.3.0"]
+                 [metrics-clojure-ring "2.3.0"]
+                 
                  [mount "0.1.9"]
                  [mrconfig "0.1.3"]
                  [org.clojure/clojure "1.8.0"]
