6444eb8872a5b07212e0ad4e553eeed3a2d2c2de
Luke Snape <ldjsnape@gmail.com>
2016-02-04 14:15:26 +0000

    Add Bidi route matching and Yada resource handling

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
   :dependencies [[aleph "0.4.1-beta2"]
                  [bidi "1.25.0"]
                  [org.clojure/clojure "1.8.0"]
-                 [mount "0.1.9"]]
+                 [org.clojure/core.cache "0.6.4"]
+                 [manifold "0.1.2-alpha2"]
+                 [mount "0.1.9"]
+                 [yada "1.1.0-20160202.093502-16"]]
   
   :profiles {:dev {:source-paths ["dev" "dev/clj"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]
