cd606bfd0dae6d4d541fbc04f0b87f83d40a484b
Luke Snape <ldjsnape@gmail.com>
2016-02-04 21:15:41 +0000

    Add metrics middleware to handler

--- src/mrs_clojure/setup.clj
+++ src/mrs_clojure/setup.clj
@@ -1,13 +1,20 @@
 (ns mrs-clojure.setup
   (:require [aleph.http :refer [start-server]]
             [bidi.ring :refer [make-handler]]
+            [metrics.ring.expose :refer [expose-metrics-as-json]]
             [mount.core :refer [defstate] :as mount]
             [mrs-clojure.config :refer [config]]
+            [mrs-clojure.middleware :refer [wrap-aleph-metrics]]
             [mrs-clojure.web :refer [routes]])
   (:gen-class))
 
+(def handler
+  (-> (make-handler routes)
+      (wrap-aleph-metrics)
+      (expose-metrics-as-json)))
+
 (defstate web-server
-  :start (start-server (make-handler routes) (:service (config)))
+  :start (start-server (make-handler handler) (:service (config)))
   :stop (.close web-server))
 
 (defn -main
