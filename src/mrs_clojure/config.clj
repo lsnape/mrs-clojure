8e10d197a4158f2a2d610b7cbcc994b1d94ff75e
Luke Snape <ldjsnape@gmail.com>
2016-02-14 20:36:03 +0000

    Add more env config vars

--- src/mrs_clojure/config.clj
+++ src/mrs_clojure/config.clj
@@ -1,11 +1,22 @@
 (ns mrs-clojure.config
-  (:require [clojure.walk :as walk]
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk]
             [environ.core :refer [env]]
             [mount.core :refer [defstate]]
-            [mrconfig.config :refer [make-config-fn]]))
+            [mrconfig.config :refer [make-config-fn]])
+  (:import [java.net InetAddress UnknownHostException]))
 
 (def ^:private config-path
   (env :app-config-path "application-config.edn"))
+
+(def ^:private hostname
+  (try
+    (-> (InetAddress/getLocalHost)
+        .getHostName
+        (str/split #"\.")
+        first)
+    (catch UnknownHostException e
+      "unknown")))
 
 (def ^:private production?
   (Boolean/valueOf (env :production "false")))
@@ -17,10 +28,13 @@
   (System/getProperty "mrs-clojure.version"))
 
 (def env-config
-  {:service {:port service-port}
+  {:box-id (env :box-id)
+   :environment-name (env :environment-name)
+   :hostname hostname
+   :service {:name (env :service-name)
+             :port service-port}
    :version version})
 
 (defstate config
-  :start (make-config-fn config-path
-                         {:env-config env-config
-                          :cached? production?}))
+  :start (make-config-fn config-path {:env-config env-config
+                                      :cached? production?}))
