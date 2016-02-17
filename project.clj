b0e30a57e291f3eb6f83e160d68d13a342e3cfe7
Luke Snape <ldjsnape@gmail.com>
2016-02-17 13:10:06 +0000

    Reorder deps

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   
   :dependencies [[aleph "0.4.1-beta2"]
                  [bidi "1.25.0"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.cache "0.6.4"]
                  [environ "1.0.2"]
                  [manifold "0.1.2-alpha2"]
                  [medley "0.7.1"]
@@ -16,15 +18,13 @@
                  [metrics-clojure-ring "2.3.0"]
                  
                  [mount "0.1.9"]
-                 [mrconfig "0.1.4"]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.cache "0.6.4"]
+                 [mrconfig "0.1.5"]
                  [org.clojure/tools.logging "0.3.1"]
                  [yada "1.1.0-20160202.093502-16"]]
   
   :profiles {:dev {:source-paths ["dev" "dev/clj"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [potemkin "0.4.3"]]
+                   :dependencies [[potemkin "0.4.3"]
+                                  [org.clojure/tools.namespace "0.2.11"]]
                    :repl-options {:init-ns user}}}
 
   :repositories {"internal-clojars" "http://clojars.brislabs.com/repo"}
